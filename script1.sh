c15fdab4a10c96a8027dfdf108b1d0ce376a9693
Olenaaa-a <durytskaolena@gmail.com>
2022-09-13 22:57:33 +0300

    Added parameters --path, --archive(if needed)

--- script1.sh
+++ script1.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-
-#Variables
-DIR_PATH=$1
-
-
+#Functions
 vm_stat () {
-    uname -a | tee $DIR_PATH/info
-    cat /etc/lsb-release | tee -a $DIR_PATH/info
-    df -h | tee $DIR_PATH/disks
-    lsblk | tee -a $DIR_PATH/disks
-    cat /etc/fstab | tee -a $DIR_PATH/disks
-    sudo fdisk -x /dev/sda | tee -a $DIR_PATH/disks
-    sudo du -ch --max-depth=1 / 2> /dev/null | tee -a $DIR_PATH/disks
-    cp /proc/cpuinfo $DIR_PATH/
-    chmod a+w $DIR_PATH/cpuinfo
-    top -n 1 | head -n 5 | tee -a $DIR_PATH/cpuinfo
+    if [ "$DIR_PATH" = "" ];
+    then
+        echo 'Path value is empty'
+        exit 1
+    fi
+
+    if [ ! -d $DIR_PATH ];
+    then
+        mkdir -p $DIR_PATH
+        if [ $? -eq 0 ];
+        then
+            uname -a | tee $DIR_PATH/info
+            cat /etc/lsb-release | tee -a $DIR_PATH/info
+            df -h | tee $DIR_PATH/disks
+            lsblk | tee -a $DIR_PATH/disks
+            cat /etc/fstab | tee -a $DIR_PATH/disks
+            sudo fdisk -x /dev/sda | tee -a $DIR_PATH/disks
+            sudo du -ch --max-depth=1 / 2> /dev/null | tee -a $DIR_PATH/disks
+            cp /proc/cpuinfo $DIR_PATH/
+            chmod a+w $DIR_PATH/cpuinfo
+            top -n 1 | head -n 5 | tee -a $DIR_PATH/cpuinfo
+        else
+            echo FAIL
+            echo "cannot create directory $DIR_PATH"
+            exit 1
+        fi
+    fi
 }
 
 
@@ -23,32 +36,36 @@ zip_vm_stat () {
     sudo tar cfz report.tar.gz $DIR_PATH/
 }
 
+#Option strings
+SHORT=p:a
+LONG=path:,archive
 
+#Reading options
+OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
 
+eval set -- "$OPTS"
 
-#Commands
-if [ "$DIR_PATH" = "" ];
-then
-    echo 'Path value is empty'
-    exit 1
-fi
+ARCHIVE=false
 
-if [ ! -d $DIR_PATH ];
-then
-    mkdir -p $DIR_PATH
-    if [ $? -eq 0 ];
-    then
-        vm_stat
-	while getopts "--archieve" option; do
-            case $option in
-                —archieve) 
-    		    zip_vm_stat
-                    exit;;
-            esac
-        done
-    else
-        echo FAIL
-        echo "cannot create directory $DIR_PATH"
-        exit 1
-    fi
-fi
+while true ; do
+    case "$1" in
+        -p | --path )
+            DIR_PATH="$2"
+            vm_stat
+            shift 2
+            ;;
+        -a | --archive )
+            ARCHIVE=true
+            zip_vm_stat
+            shift
+            ;;
+        -- )
+            shift
+            break
+            ;;
+        *)
+            echo "Enter the right PATH.\nExample of usage: sh script1.sh --path PATH_TO_DEST_DIR --archive\n\nArguments and options:\n--path                   - PATH to destination directory.\n--archive                 - option to compese destination directory."
+            exit 1
+            ;;
+    esac
+done
